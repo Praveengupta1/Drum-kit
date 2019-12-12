61cfde8b44aae4acf667680261d447d00df3ed17
Praveen Gupta <58381566+Praveengupta1@users.noreply.github.com>
2019-12-12 13:32:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,47 @@ switch(music){
 }
 });
 }
-// var audio1 = "sounds/tom-";
-//  var audio = new Audio("audio1+i+'.mp3'");
-//  audio.play();
-//
+
+
+document.addEventListener("keypress", function(event){
+  makesound(event.key)
+});
+function makesound(key){
+
+  switch(key){
+    case "w":
+              var crash = new Audio("crash.mp3");
+              crash.play();
+              break;
+    case "a":
+              var kick = new Audio("kick-bass.mp3");
+              kick.play();
+              break;
+    case "s":
+              var snare = new Audio("snare.mp3");
+              snare.play();
+              break;
+    case "d":
+              var tom1 = new Audio("tom-1.mp3");
+              tom1.play();
+              break;
+    case "j":
+              var tom2 = new Audio("tom-2.mp3");
+              tom2.play();
+              break;
+    case "k":
+              var tom3 = new Audio("tom-3.mp3");
+              tom3.play();
+              break;
+    case "l":
+              var tom4 = new Audio("tom-4.mp3");
+              tom4.play();
+              break;
+
+      default:
+              console.log(this.innerHTML);
+
+            }
+
+
+}
